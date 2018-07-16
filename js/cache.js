01cdbfbe708d43eef12c19ee94842d726733b661
Anna Moskalchuk <anna@crystax.net>
2018-07-16 20:57:16 +0300

    docs: add information to README.md fix: caches.js

--- js/cache.js
+++ js/cache.js
@@ -1,7 +1,7 @@
 const restaurantCache = 'cache-only';
 
 
-// TODO: register service worker
+//TODO: register service worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/js/cache.js').then(function(registration) {
     console.log('on', registration);
@@ -10,6 +10,8 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+
+//TODO: add files for cache
 self.addEventListener('install', function(event) {
   console.log ('I am here!');
   event.waitUntil(
@@ -25,6 +27,18 @@ self.addEventListener('install', function(event) {
   );
 });
 
+
+/*self.addEventListener('fetch', function(event) {*/
+  //event.respondWith(
+    //caches.match(event.request).then(function(response) {
+      //if (response) return response;
+      //return fetch(event.request);
+    //})
+  //);
+
+  //// TODO: respond with an entry from the cache if there is one.
+  //// If there isn't, fetch from the network.
+/*});*/
 /*self.addEventListener('fetch', function(event) {*/
   //event.respondWith(
     //caches.match(event.request)
@@ -37,29 +51,29 @@ self.addEventListener('install', function(event) {
     //)
   //);
 /*});*/
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      var fetchRequest = event.request.clone();
-      return fetch(fetchRequest).then(
-        function(response) {
-          // Check if we received a valid response
-          if(!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          var responseToCache = response.clone();
-          caches.open(restaurantCache)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
+/*self.addEventListener('fetch', function(event) {*/
+  //event.respondWith(
+    //caches.match(event.request)
+    //.then(function(response) {
+      //if (response) {
+        //return response;
+      //}
+      //var fetchRequest = event.request.clone();
+      //return fetch(fetchRequest).then(
+        //function(response) {
+          //// Check if we received a valid response
+          //if(!response || response.status !== 200 || response.type !== 'basic') {
+            //return response;
+          //}
+          //var responseToCache = response.clone();
+          //caches.open(restaurantCache)
+            //.then(function(cache) {
+              //cache.put(event.request, responseToCache);
+            //});
 
-          return response;
-        }
-      );
-      })
-    );
-});
+          //return response;
+        //}
+      //);
+      //})
+    //);
+/*});*/
