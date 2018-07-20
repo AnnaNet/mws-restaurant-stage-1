666dbedc5f776b90912cce1c0f805abae7975fcf
Anna Moskalchuk <anna@ambook.local>
2018-07-20 21:49:45 +0300

    fix: delete links

--- cache.js
+++ cache.js
@@ -13,16 +13,13 @@ let sourceCache = [
 for (let i = 1; i < 11; i++) {
   sourceCache.push(`/img/${i}.jpg`);
 };
-console.log (sourceCache);
 
 
 //TODO: add files for cache
 self.addEventListener('install', function(event) {
-  console.log ('I am here!');
   event.waitUntil(
     caches.open(rCache)
     .then(function(cache) {
-      console.log ('open cacheRest-t');
       return cache.addAll(sourceCache);
     })
   );
@@ -38,31 +35,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
-
-/*self.addEventListener('fetch', function(event) {*/
-  //event.respondWith(
-    //caches.match(event.request)
-    //.then(function(response) {
-      //if (response) {
-        //return response;
-      //}
-      //var fetchRequest = event.request.clone();
-      //return fetch(fetchRequest).then(
-        //function(response) {
-          //// Check if we received a valid response
-          //if(!response || response.status !== 200 || response.type !== 'basic') {
-            //return response;
-          //}
-          //var responseToCache = response.clone();
-          //caches.open(restaurantCache)
-            //.then(function(cache) {
-              //cache.put(event.request, responseToCache);
-            //});
-
-          //return response;
-        //}
-      //);
-      //})
-    //);
-/*});*/
